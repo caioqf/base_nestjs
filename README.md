0825b9b215d3f57d5ae3392fb66d18796d66f8a9
Caio <caio.faria@realizati.com.br>
2022-09-14 08:29:30 -0300

    ATT: Little changes on README

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Imagem base do Nest.js para agilizar a construção de projetos e POCS contendo: 
 
-- Nest.js 
+- Nest.js na **versão 9.0.0**
 - PostgreSQL
 - TypeORM
 - Docker & docker-compose
--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import {ConfigModule} from '@nestjs/config'
 
 @Module({
   imports: [
+    // Importação do módulo de configuração. Nesse caso, para carregar o .env globalmente no Nest
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true
